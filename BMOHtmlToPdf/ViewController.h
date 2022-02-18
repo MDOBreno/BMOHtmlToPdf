fe3d5fd9e88b677e5d2cfdd2a098a3f0a6ab4a1d
MDOBreno <breno.medeiros@icloud.com>
2022-02-18 16:46:06 -0300

    - Concluido

--- BMOHtmlToPdf/ViewController.h
+++ BMOHtmlToPdf/ViewController.h
@@ -7,10 +7,16 @@
 
 #import <UIKit/UIKit.h>
 
+#define kPaperSizeA4 CGSizeMake(595.2,841.8)
+
+
 @interface ViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 - (IBAction)btConverter:(id)sender;
+
+
+@property (strong, nonatomic) UIDocumentInteractionController *documentInteractionController;
 
 @end
 
