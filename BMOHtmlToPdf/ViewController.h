148237447559825467f5503caf8b57ae9971cd6c
MDOBreno <breno.medeiros@icloud.com>
2022-10-26 18:07:54 -0300

    - Atualizacao de UIWebView para WKWebView

--- BMOHtmlToPdf/ViewController.h
+++ BMOHtmlToPdf/ViewController.h
@@ -6,13 +6,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
+
 
 #define kPaperSizeA4 CGSizeMake(595.2,841.8)
 
 
 @interface ViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet WKWebView *wkWebView;
 - (IBAction)btConverter:(id)sender;
 
 
